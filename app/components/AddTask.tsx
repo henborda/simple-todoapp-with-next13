08cfdfe73da828b26688bf499c87705bc88e8ac2
henborda <henrbo@hiof.no>
2023-10-19 17:24:53 +0200

    lagt til generering av id

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,20 +1,26 @@
-"use client";
-
+"use client"
 import Modal from "./Modal";
 import { FormEventHandler, useState } from "react";
 import { addTodo } from "@/api";
 import { useRouter } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
 
 const AddTask = () => {
   const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [newTaskValue, setNewTaskValue] = useState<string>("");
+  const [uniqueIdCounter, setUniqueIdCounter] = useState<number>(1);
 
-  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+  const generateUniqueID = () => {
+    const uniqueID = `task-${uniqueIdCounter}`;
+    setUniqueIdCounter(uniqueIdCounter + 1);
+    return uniqueID;
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
+    const uniqueID = generateUniqueID();
     await addTodo({
-      id: uuidv4(),
+      id: uniqueID,
       text: newTaskValue,
     });
     setNewTaskValue("");
@@ -24,15 +30,12 @@ const AddTask = () => {
 
   return (
     <div>
-      <button
-        onClick={() => setModalOpen(true)}
-        className='btn btn-primary'
-      >
+      <button onClick={() => setModalOpen(true)} className='btn btn-primary'>
         Legg til nytt gjøremål
       </button>
 
       <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
-        <form onSubmit={handleSubmitNewTodo}>
+        <form onSubmit={handleSubmit}>
           <h3>Legg til nytt gjøremål</h3>
           <div className='modal-action'>
             <input
@@ -40,9 +43,12 @@ const AddTask = () => {
               onChange={(e) => setNewTaskValue(e.target.value)}
               type='text'
               placeholder='Skriv her'
-              className='input border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-400 '
+              className='input border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-400'
             />
-            <button type='submit' className='btn bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700'>
+            <button
+              type='submit'
+              className='btn bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700'
+            >
               Legg til
             </button>
           </div>
