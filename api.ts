fcbdea9467fa33bc8a7a48efccb7ae10a4e79dfa
henborda <henrbo@hiof.no>
2023-10-19 17:23:59 +0200

    Lagt til axios istede for fetch

--- api.ts
+++ api.ts
@@ -1,40 +1,22 @@
-import { ITask } from "./types/tasks";
+import axios from 'axios';
 
 const baseUrl = 'http://localhost:3001';
 
-
-export const getAllTodos = async (): Promise<ITask[]> => {
-  const res = await fetch(`${baseUrl}/tasks`, { cache: 'no-store' });
-  const todos = await res.json();
-  return todos;
+export const getAllTodos = async () => {
+  const response = await axios.get(`${baseUrl}/tasks`);
+  return response.data;
 }
 
-export const addTodo = async (todo: ITask): Promise<ITask> => {
-  const res = await fetch(`${baseUrl}/tasks`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(todo)
-  })
-  const newTodo = await res.json();
-  return newTodo;
+export const addTodo = async (todo: { id: string; text: string; }) => {
+  const response = await axios.post(`${baseUrl}/tasks`, todo);
+  return response.data;
 }
 
-export const editTodo = async (todo: ITask): Promise<ITask> => {
-  const res = await fetch(`${baseUrl}/tasks/${todo.id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(todo)
-  })
-  const updatedTodo = await res.json();
-  return updatedTodo;
+export const editTodo = async (todo: { id: any; text?: string; }) => {
+  const response = await axios.put(`${baseUrl}/tasks/${todo.id}`, todo);
+  return response.data;
 }
 
-export const deleteTodo = async (id: string): Promise<void> => {
-  await fetch(`${baseUrl}/tasks/${id}`, {
-    method: 'DELETE',
-  })
+export const deleteTodo = async (id: string) => {
+  await axios.delete(`${baseUrl}/tasks/${id}`);
 }
